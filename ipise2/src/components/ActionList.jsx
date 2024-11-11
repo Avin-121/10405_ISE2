10de7f052eeee8a26126ceebde242990f739352c
Avin Dabre <avindabre16@gmail.com>
2024-11-11 10:19:48 +0530

    2'ND Push

--- ipise2/src/components/ActionList.jsx
+++ ipise2/src/components/ActionList.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
 const carbons = [
-    { id: 1, name: "Use a reusable water bottle", co2Reduction: 0.5 },
-    { id: 2, name: "Take public transport", co2Reduction: 2.6 },
-    { id: 3, name: "Eat a plant-based meal", co2Reduction: 0.8 },
-    { id: 4, name: "Use energy-efficient light bulbs", co2Reduction: 0.1 },
-    { id: 5, name: "Recycle paper", co2Reduction: 0.2 },
-    ]
-
+{ id: 1, name: "Use a reusable water bottle", co2Reduction: 0.5 },
+{ id: 2, name: "Take public transport", co2Reduction: 2.6 },
+{ id: 3, name: "Eat a plant-based meal", co2Reduction: 0.8 },
+{ id: 4, name: "Use energy-efficient light bulbs", co2Reduction: 0.1 },
+{ id: 5, name: "Recycle paper", co2Reduction: 0.2 },
+]
 function ActionList({ onAddAction }) {
   const eventAdd = (carbon) => {
     onAddAction({
